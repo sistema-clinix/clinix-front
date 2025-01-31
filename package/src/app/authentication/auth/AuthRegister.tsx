9ac37de1c66a5cb38ac638c1cabceed67f7fc6bc
GeorgeLimaDev <george.lima@vtex.com.br>
2025-01-30 22:27:31 -0300

    fix nome dos campos para coincidir com backend

--- package/src/app/authentication/auth/AuthRegister.tsx
+++ package/src/app/authentication/auth/AuthRegister.tsx
@@ -6,23 +6,23 @@ import { Stack } from '@mui/system';
 
 interface User {
     role: string;
-    name: string;
+    nome: string;
     email: string;
-    password: string;
+    senha: string;
 }
 
 const AuthRegister = ({ title, subtitle, subtext }: { title?: string; subtitle?: JSX.Element | JSX.Element[]; subtext?: JSX.Element | JSX.Element[] }) => {
     const [role, setRole] = useState<string>('paciente');
-    const [name, setName] = useState<string>('');
+    const [nome, setnome] = useState<string>('');
     const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+    const [senha, setsenha] = useState<string>('');
 
     const handleRoleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setRole(event.target.value);
     };
 
     const handleRegister = async () => {
-        const user: User = { role, name, email, password };
+        const user: User = { role, nome, email, senha };
 
         try {
             const response = await fetch('http://localhost:8080/clinixSistemaUsuarios/paciente/save', {
@@ -64,20 +64,20 @@ const AuthRegister = ({ title, subtitle, subtext }: { title?: string; subtitle?:
                         <FormControlLabel value="gestor" control={<Radio />} label="Gestor" />
                     </RadioGroup>
 
-                    <Typography variant="subtitle1" fontWeight={600} component="label" htmlFor="name" mb="5px">
+                    <Typography variant="subtitle1" fontWeight={600} component="label" htmlFor="nome" mb="5px">
                         Nome
                     </Typography>
-                    <CustomTextField id="name" variant="outlined" fullWidth value={name} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setName(e.target.value)} />
+                    <CustomTextField id="nome" variant="outlined" fullWidth value={nome} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setnome(e.target.value)} />
 
                     <Typography variant="subtitle1" fontWeight={600} component="label" htmlFor="email" mb="5px" mt="25px">
                         E-mail
                     </Typography>
                     <CustomTextField id="email" variant="outlined" fullWidth value={email} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setEmail(e.target.value)} />
 
-                    <Typography variant="subtitle1" fontWeight={600} component="label" htmlFor="password" mb="5px" mt="25px">
+                    <Typography variant="subtitle1" fontWeight={600} component="label" htmlFor="senha" mb="5px" mt="25px">
                         Senha
                     </Typography>
-                    <CustomTextField id="password" variant="outlined" fullWidth type="password" value={password} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setPassword(e.target.value)} />
+                    <CustomTextField id="senha" variant="outlined" fullWidth type="senha" value={senha} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setsenha(e.target.value)} />
                 </Stack>
 
                 <Button color="primary" variant="contained" size="large" fullWidth onClick={handleRegister}>
