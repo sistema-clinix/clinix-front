f432469c9056b8f1571222d3dfa98ae45a2149d6
GeorgeLimaDev <george.lima@vtex.com.br>
2025-01-30 21:54:45 -0300

    Fix bug

--- package/src/app/list_pacientes/listagem.tsx
+++ package/src/app/list_pacientes/listagem.tsx
@@ -1,35 +1,45 @@
-
 import {
-    Typography, Box,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
-    Chip
-} from '@mui/material';
-import DashboardCard from '@/app/(DashboardLayout)//components/shared/DashboardCard';
-import { useEffect, useState } from 'react';
+  Typography,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Chip,
+} from "@mui/material";
+import DashboardCard from "@/app/(DashboardLayout)//components/shared/DashboardCard";
+import { useEffect, useState } from "react";
 
 const ListagemPacientes = () => {
-    interface Paciente {
-        id: number;
-        nome: string;
-        nomeUsuario: string;
-        enabled: boolean;
-        data: Date;
-        email: string;
-        rg: string;
-        cpf: string;
-    }
+  interface Paciente {
+    id: number;
+    nome: string;
+    nomeUsuario: string;
+    enabled: boolean;
+    data: string;
+    email: string;
+    rg: string;
+    cpf: string;
+  }
 
-    const [pacientes, setPacientes] = useState<Paciente[]>([]);
-  
-    useEffect(() => {
-      fetch("https://seu-endpoint.com/api/pacientes") // Substitua pelo seu endpoint real
-        .then((response) => response.json())
-        .then((data) => {
-          const pacientesFormatados = data.map((paciente: { id: number; nome: string; nomeUsuario: string; enabled: boolean; dataCadastro: Date; email: string; rg: string; cpf: string; }) => ({
+  const [pacientes, setPacientes] = useState<Paciente[]>([]);
+
+  useEffect(() => {
+    fetch("https://seu-endpoint.com/api/pacientes") // Substitua pelo seu endpoint real
+      .then((response) => response.json())
+      .then((data) => {
+        const pacientesFormatados = data.map(
+          (paciente: {
+            id: number;
+            nome: string;
+            nomeUsuario: string;
+            enabled: boolean;
+            dataCadastro: Date;
+            email: string;
+            rg: string;
+            cpf: string;
+          }) => ({
             id: paciente.id,
             nome: paciente.nome,
             nomeUsuario: paciente.nomeUsuario,
@@ -38,83 +48,88 @@ const ListagemPacientes = () => {
             email: paciente.email,
             rg: paciente.rg,
             cpf: paciente.cpf,
-          }));
-          setPacientes(pacientesFormatados);
-        })
-        .catch((error) => console.error("Erro ao buscar pacientes:", error));
-    }, []);
-  
-    return (
-      <DashboardCard title="Listagem geral de pacientes">
-        <Box sx={{ overflow: "auto", width: { xs: "280px", sm: "auto" } }}>
-          <Table aria-label="simple table" sx={{ whiteSpace: "nowrap", mt: 2 }}>
-            <TableHead>
-              <TableRow>
+          })
+        );
+        setPacientes(pacientesFormatados);
+      })
+      .catch((error) => console.error("Erro ao buscar pacientes:", error));
+  }, []);
+
+  return (
+    <DashboardCard title="Listagem geral de pacientes">
+      <Box sx={{ overflow: "auto", width: { xs: "280px", sm: "auto" } }}>
+        <Table aria-label="simple table" sx={{ whiteSpace: "nowrap", mt: 2 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Id
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Nome
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Username
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Enabled
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Data cadastro
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Email
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography variant="subtitle2" fontWeight={600}>
+                  RG
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography variant="subtitle2" fontWeight={600}>
+                  CPF
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {pacientes.map((paciente) => (
+              <TableRow key={paciente.id}>
                 <TableCell>
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    Id
+                  <Typography sx={{ fontSize: "15px", fontWeight: "500" }}>
+                    {paciente.id}
                   </Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    Nome
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    Username
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    Enabled
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    Data cadastro
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    Email
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    RG
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    CPF
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {pacientes.map((paciente) => (
-                <TableRow key={paciente.id}>
-                  <TableCell>
-                    <Typography sx={{ fontSize: "15px", fontWeight: "500" }}>
-                      {paciente.id}
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Box sx={{ display: "flex", alignItems: "center" }}>
-                      <Box>
-                        <Typography variant="subtitle2" fontWeight={600}>
-                          {paciente.nome}
-                        </Typography>
-                      </Box>
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Box>
+                      <Typography variant="subtitle2" fontWeight={600}>
+                        {paciente.nome}
+                      </Typography>
                     </Box>
-                  </TableCell>
-                  <TableCell>
-                    <Typography color="textSecondary" variant="subtitle2" fontWeight={400}>
-                      {paciente.enabled}
-                    </Typography>
-                  </TableCell>
-                  {/* <TableCell>
+                  </Box>
+                </TableCell>
+                <TableCell>
+                  <Typography
+                    color="textSecondary"
+                    variant="subtitle2"
+                    fontWeight={400}
+                  >
+                    {paciente.nomeUsuario}
+                  </Typography>
+                </TableCell>
+                {/* <TableCell>
                     <Chip
                       sx={{
                         px: "4px",
@@ -125,25 +140,29 @@ const ListagemPacientes = () => {
                       label={paciente.prioridade}
                     />
                   </TableCell> */}
-                  <TableCell align="right">
-                    <Typography variant="h6">{paciente.data.toLocaleDateString()}</Typography>
-                  </TableCell>
-                  <TableCell align="right">
-                    <Typography variant="h6">{paciente.email}</Typography>
-                  </TableCell>
-                  <TableCell align="right">
-                    <Typography variant="h6">{paciente.rg}</Typography>
-                  </TableCell>
-                  <TableCell align="right">
-                    <Typography variant="h6">{paciente.cpf}</Typography>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Box>
-      </DashboardCard>
-    );
-  };
-  
-  export default ListagemPacientes;
+
+                <TableCell align="right">
+                  <Typography variant="h6">{paciente.enabled}</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="h6">{paciente.data}</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="h6">{paciente.email}</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="h6">{paciente.rg}</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="h6">{paciente.cpf}</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </DashboardCard>
+  );
+};
+
+export default ListagemPacientes;
